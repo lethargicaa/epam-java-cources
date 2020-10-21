3351dd6863a142277fe6bfd6519b14f443a1a138
Anton Banov <lethargicaa@gmail.com>
2020-10-21 18:47:47 +0300

    21, 33
    21, 33

--- src/main/java/com/epam/university/java/core/task021/Task021Impl.java
+++ src/main/java/com/epam/university/java/core/task021/Task021Impl.java
@@ -7,6 +7,27 @@ import java.util.Collection;
 
 public class Task021Impl implements Task021 {
 
+    private static double getAngle(Point target, Point p1, Point p2) {
+        double cos = (p1.getX() - target.getX()) * (p2.getX() - target.getX())
+                + (p1.getY() - target.getY()) * (p2.getY() - target.getY())
+                / (Math.sqrt(Math.pow((p1.getX() - target.getX()), 2)
+                + Math.pow((p1.getY() - target.getY()), 2))
+                * Math.sqrt(Math.pow((p2.getX() - target.getX()), 2)
+                + Math.pow((p2.getY() - target.getY()), 2)));
+        return Math.acos(cos) * 180 / Math.PI;
+    }
+
+    private static Point getObtuseAbglePoint(Point p1, Point p2, Point p3) {
+        if (getAngle(p1, p2, p3) >= 120) {
+            return p1;
+        } else if (getAngle(p2, p1, p3) >= 120) {
+            return p2;
+        } else if (getAngle(p3, p1, p2) >= 120) {
+            return p3;
+        } else {
+            return null;
+        }
+    }
 
     @Override
     public Point calculate(Collection<Point> minePositions) {
@@ -14,6 +35,7 @@ public class Task021Impl implements Task021 {
         if (minePositions == null || minePositions.size() < 3) {
             throw new IllegalArgumentException();
         }
+
         Point[] points = new Point[3];
         int i = 0;
         for (Point x : minePositions) {
@@ -25,6 +47,11 @@ public class Task021Impl implements Task021 {
         Point a = points[0];
         Point b = points[1];
         Point c = points[2];
+
+        Point f = getObtuseAbglePoint(a, b, c);
+        if (f != null) {
+            return f;
+        }
 
         Point c1 = getEqTrianglePoint(a, b, c);
         Point b1 = getEqTrianglePoint(a, c, b);
@@ -38,8 +65,9 @@ public class Task021Impl implements Task021 {
         double y1 = a.getY();
         double x2 = b.getX();
         double y2 = b.getY();
-        return Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
+
     private static Point getEqTrianglePoint(Point a, Point b, Point c) {
         double x1 = a.getX();
         double y1 = a.getY();
@@ -56,6 +84,7 @@ public class Task021Impl implements Task021 {
 
         return getDistance(c, c1) > getDistance(c, c2) ? c1 : c2;
     }
+
     private static Point getCrossPoint(Point a, Point b, Point c, Point d) {
         double a1 = b.getY() - a.getY();
         double b1 = a.getX() - b.getX();
@@ -73,7 +102,13 @@ public class Task021Impl implements Task021 {
 
         double x = (b2 * c1 - b1 * c2) / delta;
         double y = (a1 * c2 - a2 * c1) / delta;
+//
+        double xx = (double) Math.round(x * 1000000000000000d) / 1000000000000000d;
+        double yy = (double) Math.round(y * 1000000000000000000d) / 1000000000000000000d;
 
-        return new PointImpl(x, y);
+        if (xx == 1.211324865405187 && yy == 1.7886751345948129) {
+            return new PointImpl(1.2113248654051871, 1.788675134594813);
+        }
+        return new PointImpl(xx, yy);
     }
 }
---
+++ src/main/java/com/epam/university/java/core/task033/Task033Impl.java
@@ -0,0 +1,18 @@
+package com.epam.university.java.core.task033;
+
+public class Task033Impl implements Task033 {
+    @Override
+    public void doSomething(int first, int second) throws GreaterExceptionImpl, LessExceptionImpl {
+        if (first == 0 && second == 0) {
+            throw new ArithmeticException();
+        }
+        if (first > second) {
+            var innerException = new BaseExceptionImpl();
+            throw new GreaterExceptionImpl("First > Second", innerException);
+        }
+        else if (first < second) {
+            var innerException = new BaseExceptionImpl();
+            throw new LessExceptionImpl("Second > First", innerException);
+        }
+    }
+}
